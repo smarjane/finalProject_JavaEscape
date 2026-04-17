56cb46b5519008cac86b8e9da1ecd326d7397c75
titiThibaud1 <thibaud.sellier@ynov.com>
2026-04-17 19:23:15 +0200

    MasterMind

--- src/main/java/com/example/javaescape/HelloApplication.java
+++ src/main/java/com/example/javaescape/HelloApplication.java
@@ -10,10 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MasterMind.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("MasterMind");
         stage.setScene(scene);
+        stage.setWidth(1200);
+        stage.setHeight(800);
+        stage.setResizable(false);
         stage.show();
     }
 }
