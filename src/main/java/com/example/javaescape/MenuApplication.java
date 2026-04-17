ca2cab8eab4408544a386a9d7e7913121be9584e
Marja <marjane.selman@ynov.com>
2026-04-17 13:00:20 +0200

    corps page princpal OK, background a suivre

--- src/main/java/com/example/javaescape/MenuApplication.java
+++ src/main/java/com/example/javaescape/MenuApplication.java
@@ -7,13 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setWidth(1200);
+        stage.setHeight(800);
+        stage.setResizable(false);
         stage.show();
     }
 }
---
+++ src/main/java/com/example/javaescape/MenuController.java
@@ -0,0 +1,28 @@
+package com.example.javaescape;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    @FXML
+    void onDemarrer(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("dialogueDebut.fxml"));//ps oubl de mettre en camelcase
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
+    @FXML
+    void onReprendre(ActionEvent event) throws Exception {
+        //+tard a faire sauvegarde
+    }
+
+    @FXML
+    void onQuitter(ActionEvent event) {
+        System.exit(0);//rev src baeldung
+    }
+}
